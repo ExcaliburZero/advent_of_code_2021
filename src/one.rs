523f30777dded6878ee5f11827832e498021a423
Christopher Wells <cwellsny@nycap.rr.com>
2021-11-30 23:51:19 -0800

    Fix clippy suggestion

--- src/one.rs
+++ src/one.rs
@@ -2,14 +2,14 @@ use std::io;
 use std::io::prelude::*;
 
 pub fn part_one() {
-    let mut numbers = read_input();
+    let numbers = read_input();
     let answer = count_num_increases(&numbers);
 
     println!("{}", answer);
 }
 
 pub fn part_two() {
-    let mut numbers = read_input();
+    let numbers = read_input();
     let answer = count_num_3_sum_increases(&numbers);
 
     println!("{}", answer);
