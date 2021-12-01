9114d100087c67c6dd293d5525781ec496b669a9
Christopher Wells <cwellsny@nycap.rr.com>
2021-11-30 23:48:21 -0800

    Solve day1 part 2

--- src/one.rs
+++ src/one.rs
@@ -10,10 +10,9 @@ pub fn part_one() {
 
 pub fn part_two() {
     let mut numbers = read_input();
-    //let answer = multiply_2020_addends(&mut numbers);
+    let answer = count_num_3_sum_increases(&numbers);
 
-    //println!("{}", answer);
-    println!("{:?}", numbers);
+    println!("{}", answer);
 }
 
 fn read_input() -> Vec<i32> {
@@ -37,6 +36,29 @@ fn count_num_increases(numbers: &[i32]) -> i32 {
             num_increases += 1;
         }
         prev = num;
+    }
+
+    num_increases
+}
+
+fn count_num_3_sum_increases(numbers: &[i32]) -> i32 {
+    let mut prev_sum: Option<i32> = None;
+    let mut num_increases = 0;
+
+    for i in 0..numbers.len() - 2 {
+        let a = numbers[i];
+        let b = numbers[i + 1];
+        let c = numbers[i + 2];
+
+        let sum = a + b + c;
+
+        if let Some(prev) = prev_sum {
+            if sum > prev {
+                num_increases += 1;
+            }
+        }
+
+        prev_sum = Some(sum)
     }
 
     num_increases
