922aa9f8ad521bfe0f288abae9e3abcd423130ec
Christopher Wells <cwellsny@nycap.rr.com>
2021-12-02 21:21:15 -0800

    Solve day3 part 2

--- src/three.rs
+++ src/three.rs
@@ -10,9 +10,9 @@ pub fn part_one() {
 
 pub fn part_two() {
     let numbers = read_input();
-    //let answer = get_final_position_2(&numbers);
+    let answer = get_life_support_rating(&numbers);
 
-    //println!("{}", answer.depth * answer.horizontal);
+    println!("{}", answer);
 }
 
 fn read_input() -> Vec<Vec<i32>> {
@@ -80,4 +80,78 @@ fn bits_to_i32(bits: &[i32]) -> i32 {
     }
 
     num
+}
+
+fn get_most_common_digit(numbers: &[Vec<i32>], position: usize) -> i32 {
+    let mut num_zeros = 0;
+    let mut num_ones = 0;
+    for number in numbers.iter() {
+        match number[position] {
+            0 => num_zeros += 1,
+            1 => num_ones += 1,
+            _ => panic!(),
+        }
+    }
+
+    if num_zeros > num_ones {
+        0
+    } else {
+        1
+    }
+}
+
+fn get_least_common_digit(numbers: &[Vec<i32>], position: usize) -> i32 {
+    let mut num_zeros = 0;
+    let mut num_ones = 0;
+    for number in numbers.iter() {
+        match number[position] {
+            0 => num_zeros += 1,
+            1 => num_ones += 1,
+            _ => panic!(),
+        }
+    }
+
+    if num_zeros > num_ones {
+        1
+    } else {
+        0
+    }
+}
+
+fn get_life_support_rating(numbers: &[Vec<i32>]) -> i32 {
+    let mut oxygen_rating_candidates: Vec<Vec<i32>> = numbers.to_vec();
+    for i in 0..numbers[0].len() {
+        if oxygen_rating_candidates.len() == 1 {
+            break;
+        }
+
+        let most_common_digit = get_most_common_digit(&oxygen_rating_candidates, i);
+
+        oxygen_rating_candidates = oxygen_rating_candidates
+            .iter()
+            .filter(|n| n[i] == most_common_digit)
+            .cloned()
+            .collect();
+    }
+
+    let oxygen_generator_rating = bits_to_i32(oxygen_rating_candidates.first().unwrap());
+
+    let mut co2_rating_candidates: Vec<Vec<i32>> = numbers.to_vec();
+    for i in 0..numbers[0].len() {
+        if co2_rating_candidates.len() == 1 {
+            break;
+        }
+
+        let least_common_digit = get_least_common_digit(&co2_rating_candidates, i);
+
+        co2_rating_candidates = co2_rating_candidates
+            .iter()
+            .filter(|n| n[i] == least_common_digit)
+            .cloned()
+            .collect();
+    }
+
+    let co2_scrubber_rating = bits_to_i32(co2_rating_candidates.first().unwrap());
+
+    oxygen_generator_rating * co2_scrubber_rating
 }
